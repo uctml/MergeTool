0488f8822dab6d519c86f959455e647d06bf9968
magatroid <uctml@naver.com>
2022-07-01 19:13:31 +0900

    first commit !!!

---
+++ MergeTool/CommandLine.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace MergeTool
+{
+    internal class CommandLine
+    {
+        private Process Cmd = null;
+        private int WaittingSec = 10000;
+
+        public void Init()
+        {
+            Cmd = new Process();
+            Cmd.StartInfo.FileName = "cmd.exe";
+            Cmd.StartInfo.RedirectStandardInput = true;
+            Cmd.StartInfo.RedirectStandardOutput = true;
+            Cmd.StartInfo.UseShellExecute = false;
+        }
+
+        public void SetDestDirectory(string Path)
+        {
+            Cmd.StartInfo.WorkingDirectory = Path;
+        }
+
+        public int Execute(string ExecuteMessage)
+        {
+            try
+            {
+                Cmd.Start();
+                Cmd.StandardInput.WriteLine(ExecuteMessage);
+                Cmd.StandardInput.Flush();
+                Cmd.StandardInput.Close();
+                Cmd.WaitForExit(WaittingSec);
+            }
+            catch(Exception ErrorException)
+            {
+                Console.WriteLine(ErrorException.Message);
+                return Cmd.ExitCode;
+            }
+            if (Cmd.HasExited == false)
+            {
+                Cmd.Kill();
+            }
+            return Cmd.ExitCode;
+        }
+
+        public string ReadToEnd()
+        {
+            return Cmd.StandardOutput.ReadToEnd();
+        }
+    }
+}
---
+++ MergeTool/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using MergeTool;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static public int Main(string[] Args)
+    {
+        if (Args.Length == 0)
+        {
+            Console.WriteLine("Please Input Argument");
+            return 0;
+        }
+        else
+        {
+            Dictionary<string, string> SwitchMappings = new Dictionary<string, string>()
+            {
+                { "-cmsg", "CommitMsgValue" },
+                { "-src", "SrcPathValue" },
+                { "-dst", "DstPathValue" },
+                { "-sr", "StartRevisionValue" },
+                { "-er", "EndRevisionValue" },
+                { "--ms", "MileStonesValue" },
+            };
+
+            IConfigurationBuilder MainBuilder = new ConfigurationBuilder().AddCommandLine(Args, SwitchMappings);
+            IConfigurationRoot MainConfig = MainBuilder.Build();
+
+            DataTemplate DTInstance = DataTemplate.Instance;
+
+            DTInstance.InitSetting(MainConfig["CommitMsgValue"], MainConfig["SrcPathValue"], 
+                MainConfig["DstPathValue"], MainConfig["StartRevisionValue"], MainConfig["EndRevisionValue"],
+                MainConfig.GetSection("ms").GetChildren().Select(x => x.Value).ToArray());
+            SvnProcesser SvnInstance = new SvnProcesser();
+            SvnInstance.Init();
+            SvnInstance.SetDestDirectory(DTInstance.DstPath);
+            SvnInstance.SearchLogsByMileStones(DTInstance.SrcPath, DTInstance.StartRevision, 
+                DTInstance.EndRevision, DTInstance.GetMileStoneArray());
+
+            switch (Args[0].ToLower())
+            {
+                    // log (DstLog)
+                case "log":
+                    SvnInstance.SvnShowLog(DTInstance.StartRevision);
+                    break;
+                    // merge
+                case "merge":
+                    SvnInstance.SvnMerge(DTInstance.SrcPath, DTInstance.DstPath, DTInstance.GetMileStoneArray());
+                    break;
+                    // merge with commit
+                case "commit":
+                    SvnInstance.SvnMergeAndCommit(DTInstance.SrcPath, DTInstance.DstPath, DTInstance.CommitMsg, DTInstance.GetMileStoneArray());
+                    break;
+                    // show merge list
+                case "showmergelist":
+                    SvnInstance.SvnShowMergeList(DTInstance.SrcPath, DTInstance.GetMileStoneArray());
+                    break;
+                default:
+                    Console.WriteLine("Input Error");
+                    break;
+            }
+        }
+        return 0;
+    }
+}
